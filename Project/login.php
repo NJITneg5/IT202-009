2a6ef4a18d7eb655cf33df2f198d24d3484be9b0
NJITneg5 <neg5@njit.edu>
2020-09-28 16:56:58 -0400

    Updated login.php

--- Project/login.php
+++ Project/login.php
@@ -61,27 +61,36 @@ body {
 			echo "<br>Invalid email<br>";
 		}
 		if($isValid){
-			//for password matching, we can't use this, every time it's ran it'll be a different value
-			//so will never log us in!
-			//$hash = password_hash($password, PASSWORD_BCRYPT);
-			//instead we'll want to run password_verify
-			//TODO pretend we got our use from the DB
-			//make sure if you're pasting a sample hash here that you use single quotes
-			//if you use double quotes it'll try to parse values with $ as a php variable
-			//and the sample won't work
-			$password_hash_from_db = '';//placeholder, you can copy/paste a hash generated from sample_reg.php if you want to test it
-			//otherwise it'll always be false
-    
-			//note it's raw password, saved hash as the parameters
-			if(password_verify($password, $password_hash_from_db)){
-				echo "<br>Welcome! You're logged in!<br>"; 
-			}
-			else{
-				echo "<br>Invalid password, get out!<br>"; 
+			require_once("db.php");
+			if(isset($db)){
+				$stmt = $db->prepare("SELECT email, password FROM TPUsers WEHRE email = :email LIMIT 1");
+				
+				$params - array":email"=>$email);
+				$r = $stmt->execute($params);
+				echo "db returned: " . var_export($r, true);
+				
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo "Something went wrong: " . var_export($e, true);
+				}
+				
+				$result = $stmt->fetch(PDO::FETCH_ASSOC);
+				if($result && isset($result["password"])){
+					$passwordHash = $result["password"];
+					if(password_verify($password, $passwordHash)){
+						echo "<br>Welcome! You're logged in!<br>";
+					}
+					else{
+						echo "<br>Invalid password, try again"
+					}
+				}
+				else{
+					echo "<br>Invalid User";
+				}
 			}
 		}
 		else{
-			echo "There was a validation issue"; 
+			echo "<br> There was a validation issue";
 		}
 	}
 ?>

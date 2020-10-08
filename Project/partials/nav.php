c0445d7d5c50d92b5f56633b19b3093e85cc7fe4
NJITneg5 <neg5@njit.edu>
2020-10-08 12:57:31 -0400

    Fixed nav.php

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -3,12 +3,12 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <ul>
-    <li><a href="../home.php">Home</a></li>
+    <li><a href="home.php">Home</a></li>
     <?php if(!is_logged_in()):?>
-        <li><a href="../login.php">Login</a></li>
-        <li><a href="../registration.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="registration.php">Register</a></li>
     <?php endif;?>
     <?php if(is_logged_in()):?>
-        <li><a href="../logout.php">Logout</a></li>
+        <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>

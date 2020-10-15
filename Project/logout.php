2d7ccc20b38f89d2351cde8065098ffb358eb089
NJITneg5 <neg5@njit.edu>
2020-10-15 12:10:34 -0400

    Fixed logout.php

--- Project/logout.php
+++ Project/logout.php
@@ -8,7 +8,7 @@
 	<meta name="date" content="9/24/2020">
 	<meta name="keywords" content="">
     <title>Gile Family Bank</title>
-    <!--<link rel="stylesheet" type="text/css" href="style.css">-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href='https://fonts.googleapis.com/css?family=Average' rel='stylesheet'>
 	<link rel="icon" href="bankIcon.jpg" type="image/gif" sizes="16x16">
 
@@ -19,7 +19,7 @@ body {
 </style>
 </head>
 <body>
-    <nav> <?php require_once(__DIR__ . "/partials/nav.php");?> </nav>
+    <?php require_once(__DIR__ . "/partials/nav.php");?>
 <?php
 	session_unset();
 	// destroy the session
@@ -31,9 +31,9 @@ body {
 	<hr>
 	
 	<address>
-	Page made by Nate Gile <br>
-	for Internet Applications Final Project. <br>
-	Created October 2020<br>
+	Page made by Nate Gile
+	for Internet Applications Final Project.
+	Created October 2020
 	</address>
 
 </body>

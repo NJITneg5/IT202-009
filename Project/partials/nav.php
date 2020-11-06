5268f71e73f9b64832278cc0e048eef8f336a20e
NJITneg5 <neg5@njit.edu>
2020-11-06 17:16:13 -0500

    Updated nav for the new test pages

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -13,7 +13,9 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <?php endif; ?>
             <?php if (has_role("Admin")): ?>
             <li style = "padding-left: 10%"><a href="<?php echo getURL("testFiles/testCreateAccounts.php");?>">Create Accounts</a></li>
-            <li style = "padding: 0"><a href="<?php echo getURL("testFiles/testListAccounts.php");?>">Query Accounts</a></li>
+            <li ><a href="<?php echo getURL("testFiles/testListAccounts.php");?>">Query Accounts</a></li>
+            <li ><a href="<?php echo getURL("testFiles/testCreateTransactions.php");?>">Query Accounts</a></li>
+            <li style = "padding: 0"><a href="<?php echo getURL("testFiles/testListTransactions.php");?>">Query Accounts</a></li>
             <?php endif; ?>
         </ul>
 

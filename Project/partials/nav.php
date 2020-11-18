ffe6d9905aebf77464fd01ab2975c10d740b0849
NJITneg5 <neg5@njit.edu>
2020-11-18 15:44:36 -0500

    Fixed typo

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li style = "padding: 0"><a href="<?php echo getURL("registration.php");?>">Register</a></li>
             <?php endif; ?>
             <?php if (is_logged_in()): ?>
-                <li style = "padding-left: 10%"><a href="<?php echo getURL("testFiles/testCreateAccounts.php");?>">Create Accounts</a></li>
+                <li style = "padding-left: 10%"><a href="<?php echo getURL("createAccount.php");?>">Create Accounts</a></li>
             <?php endif; ?>
             <?php if (has_role("Admin")): ?>
             <!--<li style = "padding-left: 10%"><a href="<?php //echo getURL("testFiles/testCreateAccounts.php");?>">Create Accounts</a></li>-->

dbd45a6cd19a1fb167138776e1384c3f5f116a07
NJITneg5 <neg5@njit.edu>
2020-11-23 16:24:11 -0500

    Fixed missed page

--- Project/logout.php
+++ Project/logout.php
@@ -1,35 +1,14 @@
-<!DOCTYPE HTML>
+<?php
+    session_start();
+    // remove all session variables
+    session_unset();
+    // destroy the session
+    session_destroy();
 
-<html lang="en">
+    require_once(__DIR__ . "/partials/nav.php");
 
-<head>
-    <meta charset="utf-8">
-	<meta name="Author" content="Nate Gile">
-	<meta name="date" content="9/24/2020">
-	<meta name="keywords" content="">
-    <title>Gile Family Bank</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Average' rel='stylesheet'>
-	<link rel="icon" href="bankIcon.jpg" type="image/gif" sizes="16x16">
-
-<style>
-body {
-    font-family: 'Average';
-}
-</style>
-</head>
-<body>
-    <?php
-        session_start();
-        // remove all session variables
-        session_unset();
-        // destroy the session
-        session_destroy();
-    ?>
-    <?php require_once(__DIR__ . "/partials/nav.php");?>
-    <?php
-        flash("You have been logged out");
-        die(header("Location: login.php"));
-    ?>
+    flash("You have been logged out");
+    die(header("Location: login.php"));
+?>
 </body>
 </html>

4b53ef3d29aee253d43642b882d71661c087db0e
NJITneg5 <neg5@njit.edu>
2020-12-02 13:52:30 -0500

    Added fix to other buttons

--- Project/listTransactions.php
+++ Project/listTransactions.php
@@ -109,7 +109,7 @@ if(isset($acctId) && isset($acctNum) && isset($balance)) {
     <div class="listNav">
         <ul class="pagination justify-content-center">
             <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-                <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+                <a class="page-link" href="id=<?php echo $acctId?>&?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
             </li>
             <?php for($i = 0; $i < $totalPages; $i++):?>
                 <li class="page-item <?php echo ($page-1) == $i?"active":"";?>">
@@ -117,7 +117,7 @@ if(isset($acctId) && isset($acctNum) && isset($balance)) {
                 </li>
             <?php endfor; ?>
             <li class="page-item <?php echo ($page+1) >= $totalPages?"disabled":"";?>">
-                <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+                <a class="page-link" href="id=<?php echo $acctId?>&?page=<?php echo $page+1;?>">Next</a>
             </li>
         </ul>
     </div>

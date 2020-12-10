7e085d16853f74393b98519bca05fb9c2303f996
NJITneg5 <neg5@njit.edu>
2020-12-10 16:58:55 -0500

    Added a die() for flashes

--- Project/profile.php
+++ Project/profile.php
@@ -135,7 +135,7 @@ if (isset($_POST["saved"])) {
             $stmt->execute([":vis" => $formVisible, ":id" => get_user_id()]);
         }
     }
-    header("Location: profile.php");
+    die(header("Location: profile.php"));
 }
 ?>
 

2eeef2af4a16b46095143e9b58f7a49fad9060ac
NJITneg5 <neg5@njit.edu>
2020-12-15 16:47:47 -0500

    Fixed typos

--- Project/admin.php
+++ Project/admin.php
@@ -91,15 +91,15 @@ if(isset($_POST["searchSub"])){
             <tbody>
             <?php foreach ($searchResults as $r):?>
                 <tr class="listRow">
-                    <td><?php safer_echo($r["user.lastName"]);?></td>
-                    <td><?php safer_echo($r["user.firstName"]);?></td>
-                    <td><?php safer_echo($r["user.enabled"]);?></td>
+                    <td><?php safer_echo($r["users.lastName"]);?></td>
+                    <td><?php safer_echo($r["users.firstName"]);?></td>
+                    <td><?php safer_echo($r["users.enabled"]);?></td>
                     <td><?php safer_echo($r["account.account_number"]);?></td>
                     <td><?php safer_echo($r["account.account_type"]);?></td>
                     <td>$<?php safer_echo($r["account.balance"]);?></td>
                     <td><?php safer_echo($r["account.active"]);?></td>
                     <td><?php safer_echo($r["account.frozen"]);?></td>
-                    <td><a href="adminCreate.php?userID=<?php echo $r["user.id"]?>">Create Checking Account for User</a>, <a href="disableUser.php?userID=<?php echo $r["user.id"]?>&enable=<?php echo $r["enabled"]?>">Enable/Disable User</a><br>
+                    <td><a href="adminCreate.php?userID=<?php echo $r["users.id"]?>">Create Checking Account for User</a>, <a href="disableUser.php?userID=<?php echo $r["users.id"]?>&enable=<?php echo $r["users.enabled"]?>">Enable/Disable User</a><br>
                         <a href="freezeAccount.php?id=<?php echo $r["account.id"]?>&frozen=<?php echo $r["frozen"]?>">(Un)Freeze Account</a><a href="adminTransactions.php?id= <?php echo $r["account.id"]?>">View Account's Transactions</a></td>
                 </tr>
             <?php endforeach;?>

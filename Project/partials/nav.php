45f8521298da7331830ecd8399b5d528a22d282a
NJITneg5 <neg5@njit.edu>
2020-12-15 16:27:52 -0500

    Added button to let admins to access the admin page

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -44,7 +44,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
                 <li><a href="<?php echo getURL("createTransaction.php");?>">Create Transaction</a></li>
             <?php endif; ?>
             <?php if (has_role("Admin")): //This is for later later test pages that I may need. ?>
-
+                <li><a href="<?php echo getURL("admin.php");?>">Admin</a></li>
             <?php endif; ?>
         </ul>
 
